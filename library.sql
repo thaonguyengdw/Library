1551d0aafa74743ef1641a58fca22a30e4eba942
Nguyen <thaonguyengdw@gmail.com>
2020-12-03 20:28:32 +0700

    updatedatabase

--- library.sql
+++ library.sql
@@ -26,11 +26,10 @@ CREATE TABLE `author` (
   `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `nickname` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `birthday` date DEFAULT NULL,
+  `birthday` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `gender` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `city` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `country` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `background` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -43,7 +42,7 @@ CREATE TABLE `author` (
 
 LOCK TABLES `author` WRITE;
 /*!40000 ALTER TABLE `author` DISABLE KEYS */;
-INSERT INTO `author` VALUES ('1','Nguyễn Nhật Ánh','Nguyễn Nhật Ánh',NULL,'Nam',NULL,'Việt Nam',NULL,NULL,NULL),('2','Joanne Kathleen Rowling','Joanne Kathleen Rowling',NULL,'Nữ',NULL,'England',NULL,NULL,NULL);
+INSERT INTO `author` VALUES ('3b35b8aa-56c2-4573-9334-f22eb722054b','Joanne K. Rowling ','Joanne K. Rowling','','Nữ','','','',''),('43ca0c8e-18ea-43cb-a3ae-499efeb737dc','Nguyễn Nhật Ánh','Nguyễn Nhật Ánh','','Nam','','VietNam','',NULL);
 /*!40000 ALTER TABLE `author` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,14 +54,14 @@ DROP TABLE IF EXISTS `book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `book` (
-  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `author_id` int DEFAULT NULL,
-  `category_id` int DEFAULT NULL,
-  `publishing_company_id` int DEFAULT NULL,
-  `publising_year` char(4) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `entry_date` date DEFAULT NULL,
-  `location` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `author_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `category_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `publishing_company_id` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `publishing_year` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `entry_date` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_book_category_idx` (`category_id`),
@@ -77,6 +76,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES ('088a5cee-4988-4de2-afc3-946e54359135','2fffffffffffff','43ca0c8e-18ea-43cb-a3ae-499efeb737dc','4b6de219-1d19-45ce-ae00-8af0a2ebe061','3333','1','','',''),('859f7811-fa29-4c42-813f-452aee408fc1','Harry Poster 5','3b35b8aa-56c2-4573-9334-f22eb722054b','8dd2ee11-7a42-4cae-949b-7e71553dde5c','69366dbd-1d60-478e-9320-7420e5c333f0','5','5','5','5'),('bc7d3f79-444d-47ca-a357-6da6cd6b409e','Harry Poster','3b35b8aa-56c2-4573-9334-f22eb722054b','8dd2ee11-7a42-4cae-949b-7e71553dde5c','69366dbd-1d60-478e-9320-7420e5c333f0','','','',''),('f985a35f-5225-438d-807e-ad07d90a6210','2ggg','3b35b8aa-56c2-4573-9334-f22eb722054b','4b6de219-1d19-45ce-ae00-8af0a2ebe061','1d07ec92-a047-41b8-9231-dab8c24070c1','1','','','');
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,7 +101,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES ('1','Truyện ngắn',NULL),('2','Tiểu thuyết',NULL),('3','Văn học',NULL),('4','Xã hội',NULL),('5','test',NULL),('8b011e60-48a3-46db-b94b-55e4e3a537da','b','b'),('8eff88cd-f56f-476c-8a3b-4b83fa85f13c','a','a'),('97f7497c-9df6-4f08-a124-7d5f1ee3fb64','c','c'),('c4a37b93-0593-4310-8c28-76b2f747805b','','');
+INSERT INTO `category` VALUES ('4b6de219-1d19-45ce-ae00-8af0a2ebe061','Xã hội',NULL),('69d36bc3-8b37-49a7-9337-917546944749','Truyện ngắn',''),('88124f18-c03f-4b44-bea1-dcbc04b9856e','Thiếu nhi',''),('8ac2f726-1031-471b-a7ad-4259b941531d','Văn học',NULL),('8dd2ee11-7a42-4cae-949b-7e71553dde5c','Tiểu thuyết','aa');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -170,9 +170,9 @@ DROP TABLE IF EXISTS `publishing_company`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `publishing_company` (
-  `id` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `phone_number` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `phone` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
@@ -185,6 +185,7 @@ CREATE TABLE `publishing_company` (
 
 LOCK TABLES `publishing_company` WRITE;
 /*!40000 ALTER TABLE `publishing_company` DISABLE KEYS */;
+INSERT INTO `publishing_company` VALUES ('1d07ec92-a047-41b8-9231-dab8c24070c1','Thanh niên','1','1','1'),('2401668c-7f78-4f98-94b7-b4770d75043b','Thanh niên','','',''),('3333','Giáo dục',NULL,NULL,NULL),('69366dbd-1d60-478e-9320-7420e5c333f0','Kim Đồng','1','1','1');
 /*!40000 ALTER TABLE `publishing_company` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -197,4 +198,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-27 20:07:18
+-- Dump completed on 2020-12-03 20:26:41
